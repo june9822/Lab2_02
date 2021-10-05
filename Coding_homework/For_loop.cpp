29a428c417a4a1414550f2c3d1ab667d3231d79d
june9822 <june9822.snu.ac.kr>
2021-10-06 00:41:52 +0900

    2017-14533

--- Coding_homework/For_loop.cpp
+++ Coding_homework/For_loop.cpp
@@ -25,9 +25,13 @@ int main()
 	int* output = new int[arrsize];
 	cout << "Array output: ";
 
-	/***********************************
-	Implement the code here!
-	************************************/
+	for (int i = 0; i < arrsize; i++) {
+		int count = 0;
+		for (int j = 0; j < arrsize; j++) {
+			if (arr[i] > arr[j]) count++;
+		}
+		output[i] = count;
+	}
 
 	for (int i = 0; i < arrsize; i++) {
 		cout << output[i] << " ";
